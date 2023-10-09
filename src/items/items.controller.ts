dd20a753737898903a475c4f9aea08eb26a6a993
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-09 17:58:34 +0900

    Add バリデーション追加

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './item.model';
 import { CreateItemDto } from './dto/create-item.dto';
@@ -12,7 +12,7 @@ export class ItemsController {
   }
 
   @Get(':id') // /items/id
-  findById(@Param('id') id: string): Item {
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.findById(id);
   }
 
@@ -22,12 +22,12 @@ export class ItemsController {
   }
 
   @Patch(':id')
-  updateStatus(@Param('id') id: string): Item {
+  updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.updateStatus(id)
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): void{
+  delete(@Param('id', ParseUUIDPipe) id: string): void{
     this.itemsService.delete(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Item } from './item.model';
 import { ItemStatus } from './item-status.enum';
 import { CreateItemDto } from './dto/create-item.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ItemsService {
@@ -17,6 +18,7 @@ export class ItemsService {
 
   create(createItemDto: CreateItemDto): Item {
     const item: Item =  {
+      id: uuid(),
       // スプレッド構文
       ...createItemDto,
       status: ItemStatus.ON_SALE
