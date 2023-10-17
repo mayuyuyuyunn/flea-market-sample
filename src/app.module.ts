855c049cdd3f66e67183fb1ca02ad4ffde2c3847
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-17 12:10:26 +0900

    Add: databaseとの接続設定追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      // Entityを作成するごとにEntityをTypeORMの設定に追加するけどそれを省略
+      autoLoadEntities: true
+    })          
+  ],
   controllers: [],
   providers: [],
 })
