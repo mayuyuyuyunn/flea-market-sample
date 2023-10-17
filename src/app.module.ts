40f7109c053c1f58518c64503b7c1ac95f965c4c
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-17 22:58:18 +0900

    Add: app.module.tsの中身を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,7 @@ import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ItemsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      // Entityを作成するごとにEntityをTypeORMの設定に追加するけどそれを省略
-      autoLoadEntities: true
-    })          
-  ],
+  imports: [ItemsModule, TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
 })
