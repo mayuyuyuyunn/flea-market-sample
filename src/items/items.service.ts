8c31d8d562993334a3804acad367028231de06f9
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-21 15:19:29 +0900

    Add&Delete: item.service.tsを変更

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Item } from './item.model';
+import { Item } from '../entities/item.entity';
 import { ItemStatus } from './item-status.enum';
 import { CreateItemDto } from './dto/create-item.dto';
-import { v4 as uuid } from 'uuid';
+import { ItemRepository } from './item.repository';
 
 @Injectable()
 export class ItemsService {
+  constructor(private readonly itemRepository: ItemRepository) {}
   private items: Item[] = [];
 
   findAll(): Item[] {
@@ -20,15 +21,8 @@ export class ItemsService {
     return found;
   }
 
-  create(createItemDto: CreateItemDto): Item {
-    const item: Item =  {
-      id: uuid(),
-      // スプレッド構文
-      ...createItemDto,
-      status: ItemStatus.ON_SALE
-    }
-    this.items.push(item);
-    return item;
+  async create(createItemDto: CreateItemDto): Promise<Item> {
+    return await this.itemRepository.createItem(createItemDto);
   }
 
   updateStatus(id: string): Item {
