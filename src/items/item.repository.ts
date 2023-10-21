c1ddb72739000f54b54a3348cdaadb04086a83d2
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-21 15:14:41 +0900

    Add: item.repository.tsにDB保存するための処理を追加

--- src/items/item.repository.ts
+++ src/items/item.repository.ts
@@ -1,7 +1,24 @@
 import { Item } from "src/entities/item.entity";
 import { EntityRepository, Repository } from "typeorm";
+import { CreateItemDto } from "./dto/create-item.dto";
+import { ItemStatus } from "./item-status.enum";
 
 @EntityRepository(Item)
 export class ItemRepository extends Repository<Item> {
+  // 非同期だからasync
+  async createItem(createItemDto: CreateItemDto): Promise<Item> {
+    const { name, price, description } = createItemDto;
+    const item = this.create({
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
 
+    await this.save(item);
+
+    return item;
+  }
 }
