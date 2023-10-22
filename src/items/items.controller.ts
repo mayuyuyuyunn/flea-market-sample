dc5c43979973d5a2420257418e1cf5701a1252d5
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-10-22 16:23:09 +0900

    Add: データベースへの保存と閲覧ができるようにした

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -6,14 +6,15 @@ import { CreateItemDto } from './dto/create-item.dto';
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  findAll(): Item[] {
-    return this.itemsService.findAll();
+  async findAll(): Promise<Item[]> {
+    return await this.itemsService.findAll();
   }
 
   @Get(':id') // /items/id
-  findById(@Param('id', ParseUUIDPipe) id: string): Item {
-    return this.itemsService.findById(id);
+  async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+    return await this.itemsService.findById(id);
   }
 
   @Post()
@@ -21,10 +22,10 @@ export class ItemsController {
     return await this.itemsService.create(createItemDto);
   }
 
-  @Patch(':id')
-  updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
-    return this.itemsService.updateStatus(id)
-  }
+  // @Patch(':id')
+  // updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
+  //   return this.itemsService.updateStatus(id)
+  // }
 
   @Delete(':id')
   delete(@Param('id', ParseUUIDPipe) id: string): void{
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -9,12 +9,12 @@ export class ItemsService {
   constructor(private readonly itemRepository: ItemRepository) {}
   private items: Item[] = [];
 
-  findAll(): Item[] {
-    return this.items;
+  async findAll(): Promise<Item[]> {
+    return await this.itemRepository.find();
   }
 
-  findById(id: string): Item {
-    const found = this.items.find((item) => item.id === id)
+  async findById(id: string): Promise<Item> {
+    const found = await this.itemRepository.findOne(id);
     if (!found) {
       throw new NotFoundException();
     }
@@ -25,11 +25,11 @@ export class ItemsService {
     return await this.itemRepository.createItem(createItemDto);
   }
 
-  updateStatus(id: string): Item {
-    const item = this.findById(id);
-    item.status = ItemStatus.SOLD_OUT
-    return item;
-  }
+  // updateStatus(id: string): Item {
+  //   const item = this.findById(id);
+  //   item.status = ItemStatus.SOLD_OUT
+  //   return item;
+  // }
 
   delete(id: string): void {
     this.items = this.items.filter((item) => item.id !== id)
